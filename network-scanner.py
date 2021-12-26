26248fb66aacc95139af8081df36b055aefff0dd
Mirsahraye <31363774+Mirsahraye@users.noreply.github.com>
2021-12-26 00:51:02 -0800

    Update network-scanner.py

--- network-scanner.py
+++ network-scanner.py
@@ -1,23 +1,34 @@
-#!/usr/bin/env python
 import scapy.all as scapy
+import argparse
 
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-t", "--target", dest="target", help="Target IP address / IP range.")
+	options = parser.parse_args()
+	if not options.target:
+		parser.error("[-] Specify target IP address / IP range, use --help for more information.")
+	return options
+	
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    clients_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        clients_list.append(client_dict)
-    return clients_list
+	arp_req = scapy.ARP(pdst=ip)
+	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+	arp_req_bcast = broadcast/arp_req
+	answered = scapy.srp(arp_req_bcast, timeout=1, verbose=False)[0]
+	
 
-def print_result(results_list):
-        print("-----------------------------------------")
-        print("IP\t\t\tMAC Address\n-----------------------------------------")
-        for client in results_list:
-                print(client["ip"] + "\t\t" + client["mac"])
-        print("------------------------------------------")
+	clients_list = []
+	for x in answered:
+		client_dict = {"ip": x[1].psrc, "mac": x[1].hwsrc}
+		clients_list.append(client_dict)
+	return clients_list
+	
 
-scan_result = scan("10.0.0.1/24")
-print_result(scan_result)
+def print_results(list_result):
+	print("IP\t\t\t\tMac Address\n----------------------------------------------------------------")
+	for client in list_result:
+		print(client["ip"] + "\t\t\t" + client["mac"])
+	
+
+options = get_args()
+scan_result = scan(options.target)
+print_results(scan_result)
